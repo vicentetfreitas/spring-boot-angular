c733a76899b0f154ad653c7f35202ca7fd5c250b
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2020-10-29 16:43:39 -0300

    Validando valores inválidos com Bean Validation

--- src/main/java/com/vicentetfreitas/springbootangular/model/Categoria.java
+++ src/main/java/com/vicentetfreitas/springbootangular/model/Categoria.java
@@ -5,14 +5,19 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "categoria")
 public class Categoria {
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+
+	@NotNull
+	@Size(min = 3, max = 20)
 	private String nome;
 
 	public Categoria() {
