bdbfd0896ae92fe3ab8301faa6477e7f8be670e3
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2020-10-30 00:45:44 -0300

    Usando eventos para adicionar header Location

--- src/main/java/com/vicentetfreitas/springbootangular/resource/PessoaResource.java
+++ src/main/java/com/vicentetfreitas/springbootangular/resource/PessoaResource.java
@@ -1,12 +1,13 @@
 package com.vicentetfreitas.springbootangular.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.vicentetfreitas.springbootangular.event.RecursoCriadoEvent;
 import com.vicentetfreitas.springbootangular.model.Pessoa;
 import com.vicentetfreitas.springbootangular.repository.PessoaRepository;
 
@@ -25,6 +26,9 @@ public class PessoaResource {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	// READ
 	public List<Pessoa> listar() {
@@ -37,11 +41,8 @@ public class PessoaResource {
 	public ResponseEntity<Pessoa> criar(@RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
 
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(pessoaSalva.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
-
-		return ResponseEntity.created(uri).body(pessoaSalva);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
 
 	/* RETONAR CATEGORIA CRIADA */
