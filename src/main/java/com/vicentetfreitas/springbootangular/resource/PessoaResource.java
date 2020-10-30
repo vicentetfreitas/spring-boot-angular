46309f002b152d0bdb0e980ce23cd55db913ff93
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2020-10-30 18:21:04 -0300

    Removendo Pessoas com DELETE

--- src/main/java/com/vicentetfreitas/springbootangular/resource/PessoaResource.java
+++ src/main/java/com/vicentetfreitas/springbootangular/resource/PessoaResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vicentetfreitas.springbootangular.event.RecursoCriadoEvent;
@@ -49,6 +51,14 @@ public class PessoaResource {
 	@GetMapping("/{codigo}")
 	public Optional<Pessoa> buscarPessoaPeloCodigo(@PathVariable Long codigo) {
 		return pessoaRepository.findById(codigo);
+	}
+
+	// DELETE
+
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		pessoaRepository.deleteById(codigo);
 	}
 
 }
